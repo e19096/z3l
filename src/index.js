fd165b3366c1d4ece0c5e9d27ba936859d71fc6d
Esther Pong <e91096@gmail.com>
2021-12-15 11:40:49 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -11,18 +11,6 @@ import Trucks from "./routes/trucks";
 import Reservations from "./routes/reservations";
 import './index.css';
 
-// const rootElement = document.getElementById("root");
-// render(
-//   <BrowserRouter>
-//     <Routes>
-//       <Route path="/" element={<App />} />
-//       <Route path="trucks" element={<Trucks />} />
-//       <Route path="reservations" element={<Reservations />} />
-//     </Routes>
-//   </BrowserRouter>,
-//   rootElement
-// );
-
 import Amplify from 'aws-amplify';
 Amplify.configure({
   Auth: {
@@ -37,13 +25,13 @@ render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="trucks" element={<Trucks />} />
-        <Route path="reservations" element={<Reservations />} />
+        <Route path="/trucks" element={<Trucks />} />
+        <Route path="/reservations" element={<Reservations />} />
         <Route
           path="*"
           element={
             <main style={{ padding: "1rem" }}>
-              <p>nothing here!</p>
+              <p></p>
             </main>
           }
         />
